6c8ab95b1c61e06945570eb70905a322cff3862b
lrcr <handsomeby@naver.com>
2019-05-20 15:28:38 +0900

    all

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
